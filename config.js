609beb4e1f27e5c7df27aee8a481a2282e50f59c
huukhanh29 <huukhanh291101@gmail.com>
2024-03-08 07:45:11 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Gửi tặng iemmm';
+const contentCard = 'Chúc Bảo My 08-03 một ngày thật đẹp và ý nghĩa, luôn được yêu thương và tràn ngập niềm vui. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'bao_my.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
